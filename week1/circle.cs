255c57c385a37bcc5f28d931f618fe1a03bc8133
timurzhanibekov <tima2000.tz21@gmail.com>
2018-02-02 13:16:22 +0600

    asd

---
+++ week1/circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class circle
+    {
+        int r;
+        public circle(int r)
+        {
+            this.r = r;
+        }
+        public static int diametr(int r)
+        {
+            return 2 * r;
+        }
+        public static double area(int r)
+        {
+            return Math.PI * r * r;
+        }
+        public static double circumference(int r)
+        {
+            return 2 * Math.PI * r; 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("diametr is - " + circle.diametr(r));
+            Console.WriteLine("area is - " + circle.area(r));
+            Console.WriteLine("circumference is - " + circle.circumference(r));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week1/rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class rectangle
+    {
+        int a;
+        int b;
+        public rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static int area(int x, int y)
+        {
+            return x * y;
+        }
+        public static int perimetr(int x, int y)
+        {
+            return 2*(x + y);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("area is - " + rectangle.area(a, b));
+            Console.WriteLine("perimetr is - " + rectangle.perimetr(a, b));
+            Console.ReadKey();
+          
+        }
+    }
+}
